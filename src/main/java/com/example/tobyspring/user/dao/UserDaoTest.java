caddff060c64482acd51c7983d7a6e58c597ca94
ragdoll <museopkim0214@gmail.com>
2021-10-16 23:49:12 +0900

    XML Configuration

--- src/main/java/com/example/tobyspring/user/dao/UserDaoTest.java
+++ src/main/java/com/example/tobyspring/user/dao/UserDaoTest.java
@@ -2,11 +2,13 @@ package com.example.tobyspring.user.dao;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class UserDaoTest {
 
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
+//        ApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDao userDao = context.getBean("userDao", UserDao.class);
 
         CountingConnectionMaker countingConnectionMaker = context.getBean("connectionMaker", CountingConnectionMaker.class);
