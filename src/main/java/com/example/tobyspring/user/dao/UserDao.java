cd8e2e6387ae6bcd722b94aef23081daaa4a561f
ragdoll <museopkim0214@gmail.com>
2021-10-24 22:12:52 +0900

    UserDao - 로컬 클래스를 활용하여 전략 패턴 개선

--- src/main/java/com/example/tobyspring/user/dao/UserDao.java
+++ src/main/java/com/example/tobyspring/user/dao/UserDao.java
@@ -22,8 +22,20 @@ public abstract class UserDao {
         this.dataSource = dataSource;
     }
 
-    public void add(User user) throws ClassNotFoundException, SQLException {
-        StatementStrategy strategy = new AddStatement(user);
+    public void add(final User user) throws ClassNotFoundException, SQLException {
+        class AddStatement implements StatementStrategy {
+
+            public PreparedStatement makePreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users (id, name, password) VALUES (?,?,?)");
+                preparedStatement.setString(1, user.getId());
+                preparedStatement.setString(2, user.getName());
+                preparedStatement.setString(3, user.getPassword());
+
+                return preparedStatement;
+            }
+        }
+
+        StatementStrategy strategy = new AddStatement();
         jdbcContextWithStatementStrategy(strategy);
     }
 
